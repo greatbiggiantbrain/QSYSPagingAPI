fcd36e397ca17a351fe3cb90a55e1f0576114d46
Brian Pratt <Brian.Pratt@qsc.com>
2018-11-01 11:26:58 -0600

    Add project files.

---
+++ CRAPI/MainWindow.xaml.cs
@@ -0,0 +1,244 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CRAPI
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public ObservableCollection<object> Log { get; set; }
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      Log = new ObservableCollection<object>();
+      BindingOperations.EnableCollectionSynchronization(Log, _itemsLock);
+      Log.CollectionChanged += Log_CollectionChanged;
+      this.DataContext = this;
+
+      // just a normal TCP connection
+      TcpClient tcp = new TcpClient("pratt110.local", 1710);
+      var s = tcp.GetStream();
+
+      int pageId = 0;
+
+      // the core needs to be pinged to keep the connection alive
+      Timer t = new Timer(new TimerCallback((st) => {
+        Rpc.Send(s, new NoOp());
+      }), null, 0, 5000);
+
+      System.Threading.Thread rxThread = new System.Threading.Thread(() =>
+      {
+        // this thread reads responses from the core
+        try
+        {
+          // json is returned
+          var obj = Rpc.ReadResponseObject(s);
+          while (obj != null)
+          {
+            try
+            {
+                JToken oMethod;
+                string method = "";
+                if (obj.TryGetValue("method", out oMethod))
+                {
+                  method = oMethod.ToString();
+                }
+                if (method == "PA.PageStatus")
+                {
+                  var prms = obj["params"];
+                  pageId = (int)prms["PageID"];
+                }
+                else if (method == "PA.ZoneStatus")
+                {
+                  ZoneStatus z = new ZoneStatus(obj);
+                  this.Dispatcher.BeginInvoke(new Action(() => { lock(_itemsLock) Log.Add(z); }));
+                }
+            }
+            catch (ThreadAbortException TAE)
+            {
+              Console.WriteLine("Read Thread LOOP Aborted");
+              break;
+            }
+            catch(ThreadInterruptedException TIE)
+            {
+              Console.WriteLine("Read Thread LOOP Interrupted");
+              break;
+            }
+            catch(Exception EX)
+            {
+              Console.WriteLine("Read Thread LOOP Exception (Continuing on) {0}", EX.Message);
+            }
+            obj = Rpc.ReadResponseObject(s);// as IDictionary<string, object>;
+          }
+        }
+        catch (ThreadAbortException TAE)
+        {
+          Console.WriteLine("Read Thread Aborted");
+        }
+        catch (ThreadInterruptedException TIE)
+        {
+          Console.WriteLine("Read Thread Interrupted");
+        }
+        catch (Exception GENX)
+        {
+          Console.WriteLine("Read Thread generic exception (bailing out) {0}", GENX.Message);
+        }
+        Console.WriteLine("LEFT WATCHER THREAD");
+      });
+      rxThread.Start(); // start the response listener
+
+      // tell the core we want to watch zone changes
+      Rpc.Send(s, new WatchEnable { Enabled = true });
+
+      // submite a test page
+      Rpc.Send(s, new PageSubmit
+      {
+        Mode = "message",
+        Originator = "Test App",
+        Description = "this is a test...",
+        Station = 5,
+        Zones = new int[] { 1, 2, },
+        ZoneTags = new string[] { },
+        Priority = 2,
+        Start = true,
+        Preamble = "",
+        Message = "Southwest_as_a_reminder.wav"
+      });
+
+      // Close everything when the app closes
+      this.Closing += (sender, e) =>
+      {
+        t.Change(0, Timeout.Infinite);
+        if (!rxThread.Join(500))
+        {
+          rxThread.Abort();
+        }
+        s.Close();
+        tcp.Close();
+
+      };
+    }
+
+    object _itemsLock = new object();
+
+
+    private void Log_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+    {
+      // scroll the log Listbox to the bottome when it changes
+      if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+      {
+        if (logBox.Items.Count > 0)
+        {
+          logBox.ScrollIntoView(logBox.Items[logBox.Items.Count - 1]);
+        }
+      }
+    }
+  }
+
+  public interface IRPCCommand
+  {
+    string Method { get; }
+  }
+
+
+  /// <summary>
+  /// Send Command to tell core to send status changes back
+  /// </summary>
+  class WatchEnable : IRPCCommand
+  {
+    public string Method
+    {
+      get
+      {
+        return "PA.ZoneStatusConfigure";
+      }
+    }
+
+    public bool Enabled { get; set; }
+    public override string ToString()
+    {
+      return base.ToString();
+    }
+  }
+
+  /// <summary>
+  /// Send Command to queue a new page
+  /// </summary>
+  class PageSubmit : IRPCCommand
+  {
+    public string Mode { get; set; }
+    public string Originator { get; set; }
+    public string Description { get; set; }
+    public int[] Zones { get; set; }
+    public string[] ZoneTags { get; set; }
+    public int Priority { get; set; }
+    public string Preamble { get; set; }
+    public string Message { get; set; }
+    public bool Start { get; set; }
+    public int Station { get; set; }
+
+    public string Method
+    {
+      get
+      {
+        return "PA.PageSubmit";
+      }
+    }
+
+    public override string ToString()
+    {
+      return string.Format("Request to play {0}", Message);
+    }
+  }
+
+  /// <summary>
+  /// Receive Command for zone status changes
+  /// </summary>
+  public class ZoneStatus
+  {
+    public ZoneStatus(JObject o)
+    {
+      Zone = o.GetValue("params").Value<int>("Zone");
+      Active = o.GetValue("params").Value<bool>("Active");
+    }
+
+    public DateTime Time { get; set; }
+    public int Zone { get; set; }
+    public bool Active { get; set; }
+
+    public override string ToString()
+    {
+      string isActive = Active ? "" : "Not";
+      return string.Format("Zone {0} is {1} Active", Zone, isActive);
+    }
+  }
+
+  /// <summary>
+  /// Send Command to ping the core
+  /// </summary>
+  class NoOp : IRPCCommand
+  {
+    public string Method
+    {
+      get
+      {
+        return "NoOp";
+      }
+    }
+
+    public override string ToString()
+    {
+        return "PING";
+      }
+    }
+}
---
+++ CRAPI/Rpc.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRAPI
+{
+  static class Rpc
+  {
+
+    static int id = 100;
+
+    public static JObject ReadResponseObject(NetworkStream stream)
+    {
+      try
+      {
+        List<byte> bytes = new List<byte>();
+        while (true)
+        {
+          byte b = (byte)stream.ReadByte();
+          if (b == 0) break;
+          bytes.Add(b);
+        }
+        string resp = Encoding.UTF8.GetString(bytes.ToArray());
+        Console.WriteLine("FROM CORE : {0}", resp);
+        
+        JObject obj = JsonConvert.DeserializeObject(resp) as JObject;
+        return obj;
+      }
+      catch (ArgumentException ex)
+      {
+        Console.WriteLine("  parse error : {0}\r\n", ex.Message);
+        return null;
+      }
+      catch (Exception)
+      {
+        return null;
+      }
+    }
+
+    public static bool ReadResponse(NetworkStream stream)
+    {
+      try
+      {
+        List<byte> bytes = new List<byte>();
+        while (true)
+        {
+          byte b = (byte)stream.ReadByte();
+          if (b == 0) break;
+          bytes.Add(b);
+        }
+        string resp = Encoding.UTF8.GetString(bytes.ToArray());
+        Console.WriteLine("FROM CORE : {0}", resp);
+        JsonConvert.DeserializeObject(resp);
+        return true;
+      }
+      catch (ArgumentException ex)
+      {
+        Console.WriteLine("  parse error : {0}\r\n", ex.Message);
+        return true;
+      }
+      catch (Exception)
+      {
+        return false;
+      }
+    }
+    private static object _writeLocker = new object();
+
+    public static void Send(NetworkStream stream, string method, object data)
+    {
+      var rpc = new
+      {
+        jsonrpc = "2.0",
+        method = method,
+        @params = data,
+        id = id++
+      };
+      string str = JsonConvert.SerializeObject(rpc);
+      Console.WriteLine("TO CORE : {0}", str);
+      byte[] bs = Encoding.UTF8.GetBytes(str);
+      lock (_writeLocker)
+      {
+        stream.Write(bs, 0, bs.Length);
+        stream.WriteByte(0); // null terminate
+      }
+    }
+    public static void Send(string ip, string method, object data)
+    {
+      if (!string.IsNullOrEmpty(ip))
+      {
+        TcpClient client = new TcpClient(ip, 1710) { ReceiveTimeout = 1000 };
+        var stream = client.GetStream();
+
+        // send logon
+        Send(stream, "Logon", new { User = "NewUser", Password = "1000" });
+
+        Send(stream, method, data);
+        // read response
+        while (ReadResponse(stream)) ;
+      }
+    }
+
+    public static void Send(NetworkStream stream, IRPCCommand command)
+    {
+      Send(stream, command.Method, command);
+    }
+  }
+}
